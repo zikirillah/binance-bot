e01feb701098c9193b80b6ae304c78e5001885aa
zikirillah <50764370+zikirillah@users.noreply.github.com>
2021-04-16 14:23:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 # Get an Key and Secret
 # https://www.binance.com/restapipub.html
 
-api_key = 'FkFoMyBts5zwmTL1khl5mBzyJuIkkOLkU5AAekHUeYkuI0rlrSwXF9yuVM9xjH94'
-api_secret = 'xF1sSNuIpuexxBs9HzTf5aTSwo7Hp6RFsaEAzBMkkrgRjpcpK1qGBLETEuhyyC6M'
+api_key = ''
+api_secret = ''
 
